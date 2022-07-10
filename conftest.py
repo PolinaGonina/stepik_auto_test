eb2a870909fd972d5bc347b5f107e5a30725f1d5
PolinaGonina <gonina1706@gmail.com>
2022-07-11 00:06:40 +0300

    initial

--- conftest.py
+++ conftest.py
@@ -1,1 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--user_language", 
+                    action='store', 
+                    default=None,
+                    help="Choose language: en, ru...")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("user_language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(10)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_basket_button(browser):
+    browser.get(link)
+    
+    assert browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary"), "нет кнопки добавления товара в корзину" 
 
